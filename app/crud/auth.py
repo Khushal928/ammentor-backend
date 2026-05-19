8b3a0c0d7611b65e452ff81e0d786a4fac2983ba
Khushal Chitturi <khushalchitturi@gmail.com>
2026-05-19 23:24:47 +0530

    feat: implement jwt auth

---
+++ app/crud/auth.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from app.db import models
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+import os
+
+key = os.environ.get("KEY")
+algorithm = os.environ.get("ALGORITHM")
+token_expire_time = 7
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="")
+
+
+def create_access_token(user_id: int) -> str:
+    expire = datetime.now(datetime.timezone.ist) + timedelta(days=token_expire_time)
+    details = {
+        "user_id": str(user_id),
+        "exp": expire
+    }
+    return jwt.encode(details, key, algorithm=algorithm)
+    
+def get_user_by_id(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def decode_token(token: str) -> dict:
+    try:
+        token_data = jwt.decode(token, key, algorithms=[algorithm])
+        return token_data
+    except jwt.ExpiredSignatureError:
+        return {"error": "token expired"}
+    except jwt.PyJWTError:
+        return {"error": "invalid token"}
